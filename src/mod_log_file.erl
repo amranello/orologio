4c71375f9e73f623d3b1156fa8df4ce3d026863b
Олег <amranello@gmail.com>
2011-05-20 10:26:57 +0300

    license

--- src/mod_log_file.erl
+++ src/mod_log_file.erl
@@ -1,3 +1,20 @@
+%% @author Oleg Krivosheev <amranello@gmail.com>
+%% @copyright 2010 Oleg Krivosheev
+
+%% Copyright 2010 Oleg Krivosheev
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
 -module(mod_log_file).
 -behaviour(gen_log).
 -export([open/1, close/0, report/2, report/3]).
--- src/orologio_utils.erl
+++ src/orologio_utils.erl
@@ -1,3 +1,20 @@
+%% @author Oleg Krivosheev <amranello@gmail.com>
+%% @copyright 2010 Oleg Krivosheev
+
+%% Copyright 2010 Oleg Krivosheev
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
 -module(orologio_utils).
 -export([get_opt/2, log_report/2, event/2, gen_name_mod/1, get_conf_all/2, get_conf_all/3, get_conf/2, get_conf/3, get_conf/1, get_elems/1, get_elems/2, check_str/1, check_atom/1, check_number/1, check_binary/1, parse_term/2, parse_term/3, gen_time_fmt/1, parse_term/1, parse_exprs/1]).
 
